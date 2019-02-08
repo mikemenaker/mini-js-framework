85688a5c17f4b3ec98ce1cc2f02cc379cb83d0ff
Mike Menaker <mikem@voicebox.com>
2019-02-07 17:01:48 -0800

    moving framework logic to framework

---
+++ src/framework.js
@@ -0,0 +1,21 @@
+import render from './vdom/render';
+import mount from './vdom/mount';
+import diff from './vdom/diff/diff';
+import { addReactivity, watcher }  from './reactivity/reactivity';
+
+export default (data, createVApp) => {
+    let vApp = createVApp();
+    const $app = render(vApp);
+
+    let $rootEl = mount($app, document.getElementById('app'));
+
+
+    addReactivity(data);
+
+    watcher(() => {
+        const vNewApp = createVApp();
+        const patch = diff(vApp, vNewApp);
+        $rootEl = patch($rootEl);
+        vApp = vNewApp;
+    })
+}
